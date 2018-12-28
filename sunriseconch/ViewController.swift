3c066312e0d6d53cfefd7187480bdb2c8aa8869c
Robert Werner <rabbitcreek@mac.com>
2018-12-27 19:43:35 -1000

    Initial Commit

---
+++ sunriseconch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sunriseconch
+//
+//  Created by RobertW. on 12/27/18.
+//  Copyright © 2018 RobertW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
