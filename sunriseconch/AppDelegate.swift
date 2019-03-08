3a4181321c9577f310aecc33430a139f0ced33b2
Robert Werner <rabbitcreek@mac.com>
2019-03-07 22:40:07 -1000

    version with GPS enabled

--- sunriseconch/AppDelegate.swift
+++ sunriseconch/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import UserNotifications
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,32 +15,73 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //Configuration
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.badge, .sound, .alert]) { (granted, error) in
+            //granted = yes, if app is authorized for all of the requested interaction types
+            //granted = no, if one or more interaction type is disallowed
+    }
+
+        //Categories - define the types of notifications that your app supports and communicate to the system how you want a notification to be presented
+        
+        //GENERAL Category
+        let generalCategory = UNNotificationCategory(identifier: "GENERAL", actions: [], intentIdentifiers: [], options: .customDismissAction)
+        
+        //INVITATION Category
+        let remindLaterAction = UNNotificationAction(identifier: "remindLater", title: "Remind me later", options: UNNotificationActionOptions(rawValue: 0))
+        let acceptAction = UNNotificationAction(identifier: "accept", title: "Accept", options: .foreground)
+        let declineAction = UNNotificationAction(identifier: "decline", title: "Decline", options: .destructive)
+        let commentAction = UNTextInputNotificationAction(identifier: "comment", title: "Comment", options: .authenticationRequired, textInputButtonTitle: "Send", textInputPlaceholder: "Share your thoughts..")
+        let invitationCategory = UNNotificationCategory(identifier: "INVITATION", actions: [remindLaterAction, acceptAction, declineAction, commentAction], intentIdentifiers: [], options: UNNotificationCategoryOptions(rawValue: 0))
+        
+        center.setNotificationCategories([generalCategory, invitationCategory])
         return true
-    }
+   
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+}
+    
+}
+    extension AppDelegate: UNUserNotificationCenterDelegate
+    {
+        //Here you get the callback for notification, if the app is in FOREGROUND.
+        //Here you decide whether to silently handle the notification or still alert the user.
+        func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
+        {
+            completionHandler([.alert, .sound]) //execute the provided completion handler block with the delivery option (if any) that you want the system to use. If you do not specify any options, the system silences the notification.
+        }
+        
+        //Here you get the callback when the user selects a custom action.
+        func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void)
+        {
+            switch response.notification.request.content.categoryIdentifier
+            {
+            case "GENERAL":
+                break
+                
+            case "INVITATION":
+                switch response.actionIdentifier
+                {
+                case "remindLater":
+                    print("remindLater")
+                    
+                case "accept":
+                    print("accept")
+                    
+                case "decline":
+                    print("decline")
+                    
+                case "comment":
+                    print("comment")
+                    
+                default:
+                    break
+                }
+                
+            default:
+                break
+            }
+            completionHandler()
+}
 }
 
--- sunriseconch/ViewController.swift
+++ sunriseconch/ViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import Solar
+import CoreLocation
+import UserNotifications
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    var latitudeMap : Double = 0.0
+    var longMap: Double = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let myLocation = CLLocationCoordinate2D(latitude: latitudeMap , longitude: longMap)
+        let someDate =  Calendar.current.date(byAdding: .day, value: -1, to: Date())
+        let solar = Solar(for: someDate!, coordinate: myLocation)
+        //let dateAsString = solar?.sunrise
+        let sunset = solar?.sunset
+       
+        
+        // Step 1: Ask for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+        }
+        
+        // Step 2: Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Invitation"
+        content.body = "Its sundset somwhere on the planet"
+        content.categoryIdentifier = "INVITATION"
+        // Step 3: Create the notification trigger
+        
+        
+        let dateComponents = Calendar.current.dateComponents([.timeZone, .year, .month, .day, .hour, .minute, .second], from: sunset!)
+        
+        print(dateComponents)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // Step 4: Create the request
+        
+        //let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: "Anniversary", content: content, trigger: trigger)
+        
+        // Step 5: Register the request
+        center.add(request) { (error) in
+            // Check the error parameter and handle any errors
+        }
+        
+        
+        
+        
+       
+ 
+       
 
 }
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0{
+            locationManager.stopUpdatingLocation()
+            longMap = location.coordinate.longitude
+            latitudeMap = location.coordinate.latitude
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
